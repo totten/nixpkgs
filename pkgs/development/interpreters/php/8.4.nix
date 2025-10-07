6ad50925d2f2ce6de52e675186a81b522e8f60d2
Tim Otten <totten@civicrm.org>
2025-10-07 00:28:45 -0700

    php84: 8.4.7 -> 8.4.13

--- pkgs/development/interpreters/php/8.4.nix
+++ pkgs/development/interpreters/php/8.4.nix
@@ -4,8 +4,8 @@ let
   base = callPackage ./generic.nix (
     _args
     // {
-      version = "8.4.7";
-      hash = "sha256-bsyxsGpN2c6jFNvnDHYtPUdlRxy26/kludowiXi/+Yg=";
+      version = "8.4.13";
+      hash = "sha256-hRgd3Kez4D8UhSGwQ71iQRlQ1GjGZ9tkAEefGxCBIZQ=";
     }
   );
 in
